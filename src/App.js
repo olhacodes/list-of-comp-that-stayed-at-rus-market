663231d298ff3819a8d189ea5b5a115be093146d
osvatukha <olha.svatukha@ubisoft.com>
2022-08-16 17:43:57 -0700

    added card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import data from './data.json';
 
+import Container from '@material-ui/core/Container';
 import Navbar from "./components/navbar";
+import CardItem from "./components/card-item";
+
+import './app.css';
 
 function App() {
 
     return (
-    <div className="App">
-      <Navbar/>
+    <div className="app">
+        <Container>
+            <Navbar/>
+               <div className="app__cards">
+                   {data.Sheet1.map(company => (
+                       <CardItem key={company.id} {...company}/>
+                   ))}
+               </div>
+       </Container>
     </div>
   );
 }
--- src/components/locallization/locallization.jsx
+++ src/components/locallization/locallization.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import {useTranslation} from 'react-i18next';
 import i18next from 'i18next';
 import cookies from 'js-cookie';
 import classNames from 'classnames';
